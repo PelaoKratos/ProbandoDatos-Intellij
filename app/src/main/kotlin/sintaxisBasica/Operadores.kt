83bf352e2b429203de0598224286fb77affab671
Servicios Digitales Concepcion <cetecom_conce@duoc.cl>
2026-08-19 10:15:25 -0400

    probando cositas

---
+++ app/src/main/kotlin/sintaxisBasica/Operadores.kt
@@ -0,0 +1,12 @@
+package org.movil.app.sintaxisBasica
+
+fun main(){
+    val x=5;
+    val y=3;
+
+    val suma = x+y;
+    println("\nLA SUMA ES $suma")
+
+    val resta=x-y
+    println("\nRESTA ES $resta")
+}
