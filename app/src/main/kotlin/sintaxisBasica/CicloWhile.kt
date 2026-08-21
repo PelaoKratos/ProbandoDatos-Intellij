d9bda24f3d89c9652a0b630363c23a11691b7aa2
Servicios Digitales Concepcion <cetecom_conce@duoc.cl>
2026-08-21 09:24:23 -0400

    Aprendiendo ciclos, for y while

---
+++ app/src/main/kotlin/sintaxisBasica/CicloWhile.kt
@@ -0,0 +1,10 @@
+package org.movil.app.sintaxisBasica
+
+fun main(){
+    val personas = listOf("Wacoldo", "Diogenes", "Wenceslao", "Amada", "Tertuliano")
+    var i=0
+    while (i<personas.size){
+        println(personas[i])
+        i++
+    }
+}
