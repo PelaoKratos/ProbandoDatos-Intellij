7b2abef387ba2f5bff321de5a841efe021e1ffb7
Servicios Digitales Concepcion <cetecom_conce@duoc.cl>
2026-08-28 09:21:34 -0400

    Creacion de Listas, Maps, Sets

---
+++ app/src/main/kotlin/sintaxisBasica/Maps.kt
@@ -0,0 +1,12 @@
+package org.movil.app.sintaxisBasica
+
+fun main(){
+    var petsMap = mutableMapOf("Michi" to "Gato","Rex" to "Perro","Firu" to "Perro")
+    println(petsMap)
+    println(petsMap["Firu"])
+    petsMap.put("Copito","Gato")
+    println(petsMap)
+    petsMap.remove("Rex")
+    println(petsMap)
+
+}
